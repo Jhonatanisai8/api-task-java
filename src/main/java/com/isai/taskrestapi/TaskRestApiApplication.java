a6db25ea2b1b7dccc7d092072e116af46597d4b8
Jhonatan <ojedasnachezjhonatan@gmail.com>
2025-01-25 08:27:43 -0500

    configuracion inicial y las dependencias necesarias

---
+++ src/main/java/com/isai/taskrestapi/TaskRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.isai.taskrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskRestApiApplication.class, args);
+    }
+
+}
