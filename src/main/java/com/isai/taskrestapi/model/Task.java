c890b869abb8afe365bbfc64c6304c739a6fa13f
Jhonatan <ojedasnachezjhonatan@gmail.com>
2025-01-26 10:29:29 -0500

    mapeo de la clase tasks

--- src/main/java/com/isai/taskrestapi/model/Task.java
+++ src/main/java/com/isai/taskrestapi/model/Task.java
@@ -3,28 +3,24 @@ package com.isai.taskrestapi.model;
 import com.isai.taskrestapi.model.enums.StatusTask;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.validator.constraints.Length;
 
-@Entity
-@Table(name = "tasks")
 @Data
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class Task extends
-        AuditModel {
+@Table(name = "tasks")
+public class Task
+        extends AuditModel {
     private static final Long serialVersionUID = 1L;
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long taskId;
 
     @NotBlank(message = "Por favor el campo tittle es obligatorio")
-    @Length(min = 10, max = 100)
+    @Length(min = 5, max = 100)
     private String tittle;
 
     @NotBlank(message = "Por favor el campo description es obligatorio")
@@ -32,7 +28,6 @@ public class Task extends
     private String description;
 
     @Enumerated(EnumType.STRING)
-    @NotBlank(message = "Por favor el campo status es obligatorio")
     private StatusTask status;
 
 }
